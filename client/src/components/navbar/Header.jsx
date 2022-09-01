ee25ff83cf683a325b44f2e8c7de48bc06aa78a0
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-09-01 18:12:22 +0530

    added navbar

---
+++ client/src/components/navbar/Header.jsx
@@ -0,0 +1,62 @@
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import { NavLink, Link } from 'react-router-dom';
+export const  Header=()=> {
+  return (
+    <>
+    <div>
+				<Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
+					<Navbar.Brand >
+						<div style={{ display: 'inline-block', paddingRight: '15px' }}>
+							<i class="fas fa-shopping-basket"></i>
+						</div>
+						Shop All
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ml-auto text-white">
+							<Nav.Link>
+								<Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+									<i class="fas fa-home"></i>Home
+								</Link>
+							</Nav.Link>
+							
+							
+
+							
+								<Nav.Link>
+									<Link to="/customer/account" style={{ textDecoration: 'none', color: 'white' }}>
+										<i class="fas fa-user-circle" style={{marginRight : '5px'}}></i>My Account
+									</Link>
+								</Nav.Link>
+							
+							<Nav.Link>
+								<Link to="/cart" style={{ textDecoration: 'none', color: 'white' }}>
+									<i class="fas fa-cart-plus"></i>
+									{1 && (
+										<span
+											style={{
+												height: '5px',
+												width: '20%px',
+												backgroundColor: 'red',
+												color: 'white',
+												marginLeft: '5.5px',
+												marginRight: '2px',
+												borderRadius: '100%',
+												padding: '3px',
+											}}
+										>
+											
+										</span>
+									)}
+									<span style={{ marginLeft: '10px' }}>Cart</span>
+								</Link>
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			</div>
+    </>
+  )
+}
+
+
